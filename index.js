5ba3e98ccc2d6a52c0d1e4cb9fe7db283950510a
samiaurmee <samia.urmee@gmail.com>
2022-11-17 15:00:57 -0500

    create middle ware

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
