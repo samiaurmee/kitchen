936da4745064de536352f5f3f1595c8b26789ede
samiaurmee <samia.urmee@gmail.com>
2022-11-18 00:40:21 -0500

    riveiw data loaded

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 app.use(cors());
@@ -32,28 +30,26 @@ async function run() {
             res.send(menus);
         });
 
-        // app.get("/menus/:id", async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) };
-        //     const menu = await menuCollection.findOne(query);
-        //     res.send(menu);
-        //   });
+        app.get("/menus/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const menu = await menuCollection.findOne(query);
+            res.send(menu);
+        });
 
-        // app.get("/reviews", async (req, res) => {
-        //     let query = {};
-        //     if (req.query.email) {
-        //       query = { email: req.query.email };
-        //     }
-        //     const cursor = reviewCollection.find(query);
-        //     const reviews = await cursor.toArray();
-        //     res.send(reviews);
-        //   });
+        app.get("/reviews", async (req, res) => {
+            let query = {};
 
-        //     app.post("/reviews", async (req, res) => {
-        //         const review = req.body;
-        //         const result = await reviewCollection.insertOne(review);
-        //         res.send(result);
-        //       });
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+        app.post("/reviews", async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        });
     }
 
     finally {
